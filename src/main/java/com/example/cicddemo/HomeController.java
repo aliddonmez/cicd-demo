8b88f21b0cc940dc71bca64f6b05c6eb426ad39b
Ali Dönmez <ali2003dnmz@gmail.com>
2026-04-16 11:30:27 +0300

    demo run

--- src/main/java/com/example/cicddemo/HomeController.java
+++ src/main/java/com/example/cicddemo/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "CI/CD sistemi çalışıyor!";
+        return "CI/CD sistemi çalışıyor2!";
     }
 
     @GetMapping("/version")
